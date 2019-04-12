557c709424b16cee1b685120af4f9674357140e5
gaylonalfano <gaylon.alfano@gmail.com>
2019-04-12 14:11:03 +0800

    Built a homepage for our project by adding a pages app. Created our first templates _base.html and home.html. Created our first views using TemplateView and configured our URLs for the project-level and for our pages app. Then we created unit tests for new homepage using SimpleTestCase and various helper functions assertTemplateUsed, reverse, resolve, SimpleTestCase.

---
+++ pages/tests.py
@@ -0,0 +1,39 @@
+# pages/tests.py
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView
+
+
+class HomePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        """Test that our homepage returns a 200 status code"""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        """Test that our homepage is using the correct template"""
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        """Test that our homepage has the correct text"""
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        """Test that our homepage does not have incorrect text"""
+        self.assertNotContains(
+            self.response,
+            'Hi there! I should not be on the page.'
+        )
+
+    def test_homepage_url_resolves_homepageview(self):
+        """Test that the name of the view matches HomePageView"""
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
